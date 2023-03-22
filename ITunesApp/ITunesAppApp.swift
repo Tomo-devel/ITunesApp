b693d36f6d75a37f008f542bf85c773e08d9e708
Tomo-dev <124762208+tomo-devl@users.noreply.github.com>
2023-03-22 13:20:35 +0900

    Initial Commit

---
+++ ITunesApp/ITunesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesAppApp.swift
+//  ITunesApp
+//
+//  Created by 井上智輝 on 2023-03-22.
+//
+
+import SwiftUI
+
+@main
+struct ITunesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
